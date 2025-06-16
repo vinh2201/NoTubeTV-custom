66509d726aea7adde8d1141afc2f3a0ae4d93fa3
Nguyễn Tấn Vĩnh <63141930+vinh2201@users.noreply.github.com>
2025-06-16 18:11:59 +0700

    update new

--- app/src/main/java/com/ycngmn/notubetv/utils/fetchScripts.kt
+++ app/src/main/java/com/ycngmn/notubetv/utils/fetchScripts.kt
@@ -6,7 +6,7 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 
-const val SCRIPTS_URL = "https://raw.githubusercontent.com/ycngmn/NoTubeTV/refs/heads/main/assets/userscripts.js"
+const val SCRIPTS_URL = "https://raw.githubusercontent.com/vinh2201/NoTubeTV-custom/refs/heads/0.0.2-old/assets/userscripts.js"
 suspend fun fetchScripts(): String {
     val httpClient = HttpClient(CIO)
     val response: HttpResponse = httpClient.get(SCRIPTS_URL)
